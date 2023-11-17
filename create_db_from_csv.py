431be023cf1827ba65bd7e7b603765c6e487a2eb
sisira <sisira3128@gmail.com>
2023-11-17 17:31:31 -0500

    Created database

---
+++ create_db_from_csv.py
@@ -0,0 +1,22 @@
+
+
+import os
+import pandas as pd
+import sqlite3
+
+
+input_path = r"C:\Users\kbala\OneDrive\Documents\Projects\SP_Stock_Price_Analysis\csv_data"
+files_in_path = os.listdir(input_path)
+
+#Create and connect to Sqlite database
+connection = sqlite3.connect('stockprice.db')
+
+#Creating table names from csv folder
+for tableName in files_in_path:
+    tableName = tableName[:-4]
+    
+# Creating the tables into our database through SQLite connection 
+    df = pd.read_csv(input_path+"\\"+tableName+".csv")
+    df.to_sql(tableName,connection,if_exists="replace")
+
+connection.close()
